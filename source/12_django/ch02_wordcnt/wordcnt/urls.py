65adac2b5b888f170faf1d139aa0f5424313e6f6
Hyo0806 <rlagyrud9417@gmail.com>
2026-01-16 11:23:19 +0900

    Django ch02 POST

--- source/12_django/ch02_wordcnt/wordcnt/urls.py
+++ source/12_django/ch02_wordcnt/wordcnt/urls.py
@@ -4,12 +4,12 @@
 # wordcnt/result/: name=wordcnt:result
 
 from django.urls import path
-from wordcnt.views import wordinput
+from wordcnt.views import wordinput, about, result
 app_name = 'wordcnt' # name=wordcnt:wordinput을 name='wordinput'만 칠 수 있음
 
 # 여기까지 왔다는건 이미 wordcnt/하고 온거라서 앞에 wordcnt 안치기
 urlpatterns = [
-    path('', wordinput, name='wordinput'),   # /wordcnt/ 단어입력 받는 페이지
-    # path('about/', about, name='about'),   # /wordcnt/about/ 도움말
-    # path('result/', result, name='result') # /wordcnt/result/ 단어입력결과
+    path('', wordinput, name='wordinput'), # /wordcnt/ 단어입력 받는 페이지
+    path('about/', about, name='about'),   # /wordcnt/about/ 도움말
+    path('result/', result, name='result') # /wordcnt/result/ 단어입력결과
 ]
--- source/12_django/ch02_wordcnt/wordcnt/views.py
+++ source/12_django/ch02_wordcnt/wordcnt/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def wordinput(request):
     return render(request, 'wordcnt/wordinput.html')
+
+def about(request):
+    return render(request, 'wordcnt/about.html')
+
+def result(request):
+    # print(request.GET)
+    # fulltxt = request.GET['fulltxt']
+    # fulltxt = request.GET.get('fulltxt', '') # 홍길동 홍길동 아자
+    fulltxt = request.POST.get('fulltxt','')
+    strlength = len(fulltxt) # 글자수(10)
+    words = fulltxt.split() # space 단위로 단어 분리 ['홍길동', '홍길동', '아자']
+    words_dict = dict() # 빈딕셔너리 → {'홍길동':2, '아자':1}
+    for word in words:
+        if word in words_dict.keys():
+            words_dict[word] += 1 # {'홍길동' : 2}
+        else:
+            words_dict[word] = 1 # {'홍길동':1, '아자':1}
+        # {'홍길동':2, '아자':1}
+    context = {
+        'fulltxt'  : fulltxt,
+        'strlength': strlength,   # 글자수
+        'wordcnt'  : len(words),  # 단어 갯수
+        'dict'     : words_dict.items() # [('홍길동',2), ('아자',1)]
+    }
+    # print(words_dict.items())
+    return render(request=request,
+                template_name='wordcnt/result.html', 
+                context=context)
